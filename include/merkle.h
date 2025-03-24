8c9bddbdda09f3f336dcb7ab9a1aa99383c00d87
dashindia45 <dkroyalsolanki@gmail.com>
2025-03-24 15:09:52 +0530

    merkel tree integrated working on data display formate

---
+++ include/merkle.h
@@ -0,0 +1,39 @@
+#ifndef MERKLE_H
+#define MERKLE_H
+
+#include <vector>
+#include <string>
+#include <mutex>
+
+class MerkleTree {
+private:
+    struct Node {
+        std::string hash;
+        std::string userHash;
+        std::string voteHash;
+        Node* left;
+        Node* right;
+        
+        Node(const std::string& h) : hash(h), left(nullptr), right(nullptr) {}
+        ~Node() {}
+    };
+    
+    Node* root;
+    std::vector<Node*> leaves;
+    std::mutex tree_mutex;
+    
+    // Helper methods
+    Node* buildTreeFromLeaves();
+    void clearInternalNodes();
+    
+public:
+    MerkleTree() : root(nullptr) {}
+    ~MerkleTree();
+    
+    void addVote(const std::string& userHash, const std::string& voteHash);
+    std::string getRootHash();
+    int getLeafCount();
+    std::string serializeToJson();
+};
+
+#endif
